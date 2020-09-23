682563ddcf49e215288909c1ee150ce35c4a2cbc
Solène <solene.garda.krebs@gmail.com>
2020-09-23 19:16:05 +0200

    Added import of FileType in Cyano admin

--- src/Admin/CyanoAdmin.php
+++ src/Admin/CyanoAdmin.php
@@ -14,6 +14,7 @@ use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
