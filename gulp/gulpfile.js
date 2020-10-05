85ed7867f8feb9b732eba780ac0366b281f18ca2
Solène <solene.garda.krebs@gmail.com>
2020-10-05 16:50:05 +0200

    Modifications to use CDN Bootstrap and custom css

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -3,24 +3,19 @@ const { src, dest } = require('gulp');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
-const purgeCss = require('gulp-purgecss');
-
 const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 
 const cleanCss = require('gulp-clean-css');
 
-const scss_file_to_compile_path = './node_modules/bootstrap/scss/bootstrap.scss';
+const scss_file_to_compile_path = './scss/main.scss';
 const css_dest_file_path = '../public/css/';
 
 function compileScss(cb) {
 	return src(scss_file_to_compile_path)
 		.pipe(sass())
-		.pipe(purgeCss({
-			content: ['../templates/**/*.html.twig']
-		}))
 		.pipe(postcss([ autoprefixer() ]))
-		.pipe(cleanCss())
+//		.pipe(cleanCss())
 		.pipe(dest(css_dest_file_path));
 }
 
